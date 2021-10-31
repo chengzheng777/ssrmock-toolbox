7b6900a3f2ccbea1111a27f4bd049de85edb3b97
程政 <60911364+chengzheng777@users.noreply.github.com>
2021-10-31 18:56:09 +0800

    修改配置

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,11 +1,7 @@
 <template>
-	<view class="home-container">
+	<view class="content">
 		<view class="text-area">
-			<input v-model="rgbValue" />
-			<view :style="`color: ${rgbValue};`">ceshi</view>
-			<button @click="sumbit()">开启转换</button>
 			<text class="title">{{title}}</text>
-			<view :style="`color: ${title};`">ceshi</view>
 		</view>
 	</view>
 </template>
@@ -14,45 +10,15 @@
 	export default {
 		data() {
 			return {
-				rgbValue: 'rgb(80, 247, 255)',
-				title: '初始空白',
+				title: 'Hello'
 			}
 		},
-		onLoad() {},
-		methods: {
-			// 点击按钮
-			sumbit() {
-				this.title = this.colorHex(this.rgbValue);
-			},
-			// rgb 转 16
-			colorHex(value) {
-				// 16进制颜色的正则表达式
-				const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
-				// 开始匹配 
-				if (/^(rgb|RGB)/.test(value)) { // 查找是否包含rgb字段
-					let strHex = "#"; // 初始目标值
-					let colorArr = value.replace(/(?:\(|\)|rgb|RGB)/g, "").split(","); // 先删除 括号+rgb 再转成数组
-					for (const item of colorArr) {
-						let hex = Number(item).toString(16); // 转成 16 进制
-						hex = hex.length < 2 ? `0${hex}` : hex; // 判断只有1位的情况
-						strHex += hex; // 累加
-					};
-					console.log('打印', strHex)
-					return strHex;
-				}
-			}
+		onLoad() { 
+		},
+		methods: { 
 		}
 	}
 </script>
 
-<style lang="scss" scoped>
-	.home-container {
-		position: relative;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		flex-flow: column nowrap;
-		justify-content: flex-start;
-		align-items: center;
-	}
+<style> 
 </style>
--- pages/work-bench/index.vue
+++ pages/work-bench/index.vue
@@ -1,7 +1,9 @@
 <template>
-	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="home-container">
+		<view class="">
+			<input v-model="rgbValue" />
+			<button @click="sumbit()">开启转换</button>
+			<text class="">{{title}}</text>
 		</view>
 	</view>
 </template>
@@ -10,40 +12,45 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				rgbValue: 'rgb(80, 247, 255)',
+				title: '初始空白',
 			}
 		},
-		onLoad() { 
-		},
-		methods: { 
+		onLoad() {},
+		methods: {
+			// 点击按钮
+			sumbit() {
+				this.title = this.colorHex(this.rgbValue);
+			},
+			// rgb 转 16
+			colorHex(value) {
+				// 16进制颜色的正则表达式
+				const reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+				// 开始匹配 
+				if (/^(rgb|RGB)/.test(value)) { // 查找是否包含rgb字段
+					let strHex = "#"; // 初始目标值
+					let colorArr = value.replace(/(?:\(|\)|rgb|RGB)/g, "").split(","); // 先删除 括号+rgb 再转成数组
+					for (const item of colorArr) {
+						let hex = Number(item).toString(16); // 转成 16 进制
+						hex = hex.length < 2 ? `0${hex}` : hex; // 判断只有1位的情况
+						strHex += hex; // 累加
+					};
+					console.log('打印', strHex)
+					return strHex;
+				}
+			}
 		}
 	}
 </script>
 
-<style>
-	.content {
+<style lang="scss" scoped>
+	.home-container {
+		position: relative;
+		width: 100%;
+		height: 100%;
 		display: flex;
-		flex-direction: column;
+		flex-flow: column nowrap;
+		justify-content: flex-start;
 		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
 	}
 </style>
