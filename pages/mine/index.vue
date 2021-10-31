9220923fa7884ca1ea5302b5bbaaf4a915f43147
程政 <60911364+chengzheng777@users.noreply.github.com>
2021-10-31 15:15:08 +0800

    首页的开始
    1： 新建首页，工作台，我的
    2：怎加gitignore

---
+++ pages/mine/index.vue
@@ -0,0 +1,62 @@
+<template>
+	<view class="content">
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+			this.getPhoneInfo();
+		},
+		methods: {
+			// 获取设备信息
+			getPhoneInfo() {
+				uni.getSystemInfo({
+					success: (res) => {
+						console.log('获取设备信息>>>', res)
+					},
+					fail: (error) => {
+						console.log('获取设备信息失败>>>', error)
+					}
+				});
+
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
