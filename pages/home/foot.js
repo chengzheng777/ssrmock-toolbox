fc3c7936122395a11e84ed60f7a7ea058eda1177
程政 <60911364+chengzheng777@users.noreply.github.com>
2021-12-22 19:09:40 +0800

    预备新增食谱

---
+++ pages/home/foot.js
@@ -0,0 +1,32 @@
+const foodList = [{
+		value: '0',
+		name: '捞面'
+	}, {
+		value: '1',
+		name: 'KFC'
+	}, {
+		value: '2',
+		name: '包子'
+	},
+	{
+		value: '3',
+		name: '烤鱼'
+	}, {
+		value: '4',
+		name: '热干面'
+	}, {
+		value: '5',
+		name: '肠粉'
+	}, {
+		value: '6',
+		name: '湖南米粉'
+	}, {
+		value: '7',
+		name: '盖浇饭'
+	}, {
+		value: '8',
+		name: '重庆小面'
+	}
+]
+
+export default foodList;
--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,24 +1,49 @@
+<!--
+ * @Description: 食物随机选择
+ * @Author: 
+ * @Date: 
+ * @Editors: 
+ * @LastEditTime: 
+-->
 <template>
-	<view class="content">
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="container">
+		<view class="show-list">
+			<view v-for="(item, index) in show_list" :key="item.value">
+				{{ item.name }}
+			</view>
 		</view>
+		<view class="value">
+			{{ value }}
+		</view>
+		<button>开始</button>
 	</view>
 </template>
 
 <script>
+	import foodList from "./foot.js";
 	export default {
+		name: '',
+		components: {},
+		filters: {},
+		props: {},
 		data() {
 			return {
-				title: 'Hello'
+				value: '点击开始选择',
+			};
+		},
+		computed: {
+			show_list() {
+				return foodList;
 			}
 		},
-		onLoad() { 
+		methods: {},
+		watch: {},
+		onLoad(options) {},
+		onReady() {
+			console.log('---', foodList)
 		},
-		methods: { 
-		}
 	}
 </script>
 
-<style> 
+<style lang="scss" scoped>
 </style>
